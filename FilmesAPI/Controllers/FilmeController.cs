2e1b3c25716d4cbc414d29878dac6595bb6217d1
Guilherme <guilhermegomes07@hotmail.com>
2021-08-23 19:49:55 -0300

    Primeiro Commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        /*private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;*/
+
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpPost] //Criar um recurso
+        public IActionResult AdcionaFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            /*filme.Id = id++;
+            filmes.Add(filme);*/
+            //Console.WriteLine(filme.Titulo);
+
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();//Salvar alterações no banco
+            return CreatedAtAction(nameof(RetornaFilmePorID), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet] //Recuperar um recurso
+        public IEnumerable<Filme> RetornaFilmes()
+        {
+            //return Ok(filmes);
+            return(_context.Filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RetornaFilmePorID(int id)
+        {
+            //Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+                return Ok(filmeDto);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(filmeDto, filme); //sobrescrevendo as informãções de filmeDto e jogando em filme
+            _context.SaveChanges();
+            return NoContent();
+            
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+
+        public string Genero { get; set; }
+
+        [Range(45, 300, ErrorMessage = "A duração deve ter entre 45 e 300 minutos")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
